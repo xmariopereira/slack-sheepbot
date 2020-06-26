ec644582a697347edd61815087a6426227ed243e
Mario Pereira <mario.pereira@outlook.pt>
2020-06-26 14:40:18 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,15 @@
-from slack import RTMClient
+from slackclient import SlackClient
 import os
 import time
-import boto3
 
 token = os.environ.get('SLACKBOT_MYSHIP_TOKEN')
-client = RTMClient(token=slack_token)
+slack_client = SlackClient(token)
 
 link = '<https://myShipPicture>'
 
-if client.rtm_connect():
+if slack_client.rtm_connect():
     while True:
-        events = client.rtm_read()
+        events = slack_client.rtm_read()
         for event in events:
             if (
                 'channel' in event and
@@ -20,21 +19,21 @@ if client.rtm_connect():
                 channel = event['channel']
                 text = event['text']
                 if 'ship' in text.lower() and link not in text:
-                    client.api_call(
+                    slack_client.api_call(
                         'chat.postMessage',
                         channel=channel,
                         text=link,
                         as_user='true:'
                     )
                 elif 'cheap' in text.lower() and link not in text:
-                    client.api_call(
+                    slack_client.api_call(
                         'chat.postMessage',
                         channel=channel,
                         text=link,
                         as_user='true:'
                     )
                 elif 'sheep' in text.lower() and link not in text:
-                    client.api_call(
+                    slack_client.api_call(
                         'chat.postMessage',
                         channel=channel,
                         text=link,
